5779f4ef8afe214c1580e744f97637bfd248432b
Gabriel <gabriel.ulloa.e@gmail.com>
2019-11-19 09:23:49 -0300

    core: apollo server configs

--- src/server.js
+++ src/server.js
@@ -3,13 +3,45 @@ const typeDefs = gql(require("./graphql/schema"));
 const resolvers = require("./graphql/resolvers");
 const db = require("./models");
 
+/*
+ * Get operation name of the current request
+ * Usefull if want to check which resolvers must be authenticated
+ *
+ * There are two cases:
+ * - Multiple definitions: use the operation name of the request to find the real resolver name
+ * - Unique definition: use the unique given resolver name
+ */
+const getOperationNameFromDoc = ({ doc, req }) => {
+  const { definitions } = doc;
+  const operationName =
+    definitions.length > 1
+      ? definitions.find(
+          element => element.name.value === req.body.operationName
+        ).selectionSet.selections[0].name.value
+      : definitions[0].selectionSet.selections[0].name.value;
+  return operationName;
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    console.log("request from ip" + req.ip);
+    if (req.method === "get") return null;
+    const doc = gql(req.body.query);
+    const operationName = getOperationNameFromDoc({ doc, req });
+    console.log({ operationName });
     return { db };
-  }
+  },
+  formatError: err => {
+    // TODO: handle internal server errors
+    console.log(err);
+    return err;
+  },
+  cors: true,
+  introspection: process.env.NODE_ENV !== "production",
+  playground: process.env.NODE_ENV !== "production",
+  debug: process.env.NODE_ENV !== "production",
+  tracing: process.env.NODE_ENV !== "production"
 });
 
 module.exports = {
