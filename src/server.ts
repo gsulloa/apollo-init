126ad489d84a135327516b2dd75c77cc61918776
Gabriel <gabriel.ulloa.e@gmail.com>
2020-01-23 19:32:26 -0300

    refactor: modularize apollo server construct

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,13 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
 import { buildFederatedSchema } from '@apollo/federation';
 import resolvers from './graphql/resolvers';
 import typeDefs from './graphql/schema';
-import { Context } from './models/context.interface';
-import { User } from './models';
-import { GraphQLError, OperationDefinitionNode, FieldNode } from 'graphql';
-/*
- * Get operation name of the current request
- * Usefull if want to check which resolvers must be authenticated
- *
- * There are two cases:
- * - Multiple definitions: use the operation name of the request to find the real resolver name
- * - Unique definition: use the unique given resolver name
- */
-export const getOperationNames = ({ req }): string[] => {
-  const doc = gql(req.body.query);
-  const { definitions } = doc;
-  const operationNames: string[] =
-    definitions.length > 1
-      ? (definitions as OperationDefinitionNode[])
-        .find(element => element.name.value === req.body.operationName)
-        .selectionSet.selections.map(
-          (selection: FieldNode) => selection.name.value,
-        )
-      : (definitions as OperationDefinitionNode[])[0].selectionSet.selections.map(
-        (selection: FieldNode) => selection.name.value,
-      );
-  return operationNames;
-};
+import { transformInternalErrors, buildContext } from './utils/apollo.helpers';
 
 const server = new ApolloServer({
   schema: buildFederatedSchema({ typeDefs, resolvers }),
-  context: ({ req }): Context => {
-    if (req.method === 'get') return null;
-    const operationNames = getOperationNames({ req });
-    console.log({ operationNames }); // eslint-disable-line no-console
-    return { getUser: (): User => new User() };
-  },
-  formatError: (err): GraphQLError => {
-    // TODO: handle internal server errors
-    console.log(err); // eslint-disable-line no-console
-    return err;
-  },
+  context: buildContext,
+  formatError: transformInternalErrors,
   cors: true,
   introspection: process.env.NODE_ENV !== 'production',
   playground: process.env.NODE_ENV !== 'production',
@@ -53,5 +19,5 @@ export const listen = ({
   path = '/graphql',
 } = {}): Promise<void> =>
   server.listen({ port, path }).then(({ url }): void => {
-    console.log(`🚀 Server ready at ${url}`); // eslint-disable-line no-console
+    console.log(`🚀 Server ready at ${url}`);
   });
